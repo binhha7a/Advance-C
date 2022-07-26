2ca29b55de845a223519742c87c0cb348ba13ab2
binh <binhha7a@gmail.com>
2022-07-27 02:00:27 +0700

    update Stack.c

--- Stack.c
+++ Stack.c
@@ -12,42 +12,24 @@ typedef struct stack_data {
 Stack init_stack(int temp){
     top = -1;
     size = temp;
-    Stack p = NULL;
+    Stack p = malloc(sizeof(struct stack_data));
+    p->position = -1;
+    p->next = NULL;
     return p;
 }
 void push(Stack node, int value)
 {
-    // if (top == size){
-    //     printf("Stack is full, cannot push");
-    // } else {
-    //     Stack p = node;
-    //     if (p == NULL){
-    //         top++;
-    //         p->data = value;
-    //         p->position = top;
-    //         p->next = NULL;
-    //     } else {
-    //         while (p->next != NULL){
-    //             p = p->next;
-    //         }
-        
-    //     top++;
-    //     Stack new;
-    //     p->next = new;
-    //     new->data = value;
-    //     new->position = top;
-    //     }
-    // }
     Stack p = node;
-    if (p == NULL) {
+    if (p->position == -1) {
         top++;
         p->data = value;
-        p->position = top;
+        p->position=top;
+        p->next = NULL;
     } else {
         while (p->next != NULL){
             p = p->next;
         }
-        Stack new;
+        Stack new = malloc(sizeof(struct stack_data));
         top++;
         new->data = value;
         new->position = top;
@@ -55,12 +37,12 @@ void push(Stack node, int value)
         p->next = new;
     }
 };
-void pull(Stack node){ 
+void pull(Stack node){
     Stack p = node;
     Stack temp;
     if (top > -1){
         if (p->next == NULL){
-            p = NULL;
+            free(p);
             top--;
         } else {
             while (p->next != NULL){
@@ -68,7 +50,7 @@ void pull(Stack node){
                 p = p->next;
             }
             temp->next = NULL;
-            p = NULL;
+            free(p);
             top--;
         }
     }
@@ -93,22 +75,25 @@ void Show_all_node(Stack node){
     Stack p = node;
     if (p == NULL){
         printf("Khong co phan tu");
-    
+
     } else {
-    printf("Node %d = %d\n",i,p->data);
+    printf("Node %d = %d, position %d,next = %x, add = %x\n",i,p->data,p->position,p->next,p);
     while (p->next != NULL){
         p = p->next;
         i++;
-        printf("Node %d = %d\n",i,p->data);
+        printf("Node %d = %d, position %d,next = %x, add =%x\n",i,p->data,p->position,p->next,p);
     };
     }
 }
 int main(int argc, char const *argv[])
 {
-    printf("Enter size of stack ");
-    scanf("%d",&size);
+    //printf("Enter size of stack ");
+    //scanf("%d",&size);
+    size = 5;
     Stack vt = init_stack(size);
     push(vt,100);
+    push(vt,111);
+    printf("data = %d,position = %d , add= %x\n",vt->data,vt->position,vt);
     Show_all_node(vt);
     return 0;
 }
